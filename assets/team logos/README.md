030d546ba4913a819e049692edd47ed334353204
Michael Taylor <michael.a.taylor@uconn.edu>
2021-02-07 17:43:23 -0500

    implement logo web-scraper

---
+++ assets/team logos/README.md
@@ -0,0 +1,20 @@
+# NCAA Team Logo Web Scraper
+
+A python script which creates a json file containing references to logo images for each team participating in NCAA mens basketball.
+
+## Uses
+Will be used to display associated team logos on front-end interface of NLMB project.
+
+## Status
+Works for ~85% of NCAA Men's Basketball teams. Still under **active development**.
+
+## Installing Dependancies
+```
+pip3 install -r requirements.txt
+```
+
+## How to run
+```
+python3 logo-scraper.py
+```
+**Output file:** ```logos.json```
---
+++ assets/team logos/logo-scraper.py
@@ -0,0 +1,60 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from sportsreference.ncaab.teams import Teams
+
+logo_urls = {}
+teams = Teams()
+URL = 'https://www.ncaa.com/schools/'
+
+worked = 0
+failed = 0
+
+for team in teams:
+    name = team.name
+    formatted_name = name.replace(' ', '-').replace('.', '').replace('\'', '').lower()
+
+    try:
+        page = requests.get(URL + formatted_name)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        results = soup.find(id='block-bespin-content')
+        if not results:
+            if formatted_name.find('&-') > -1:
+                page = requests.get(URL + formatted_name.replace('&-', ''))
+            elif formatted_name.find('&') > -1:
+                page = requests.get(URL + formatted_name.replace('&', ''))
+            elif formatted_name.find('state') > -1:
+                page = requests.get(URL + formatted_name.replace('state', 'st'))
+            elif formatted_name.find('(') > -1:
+                page = requests.get(URL + formatted_name.replace('(', '').replace(')',''))
+            elif formatted_name.find('florida') > -1:
+                page = requests.get(URL + formatted_name.replace('florida', 'fla'))
+            elif formatted_name.find('illinois') > -1:
+                page = requests.get(URL + formatted_name.replace('illinois', 'ill'))
+            elif formatted_name.find('kentucky') > -1:
+                page = requests.get(URL + formatted_name.replace('kentucky', 'ky'))
+            elif formatted_name.find('north-carolina') > -1:
+                page = requests.get(URL + formatted_name.replace('north-carolina', 'unc'))
+            elif formatted_name.find('-') > -1:
+                i = formatted_name.find('-')
+                page = requests.get(URL + formatted_name[:i+5])
+            soup = BeautifulSoup(page.content, 'html.parser')
+            results = soup.find(id='block-bespin-content')
+        
+        job_elems = results.find_all('div', class_='school-logo')
+
+        for job_elem in job_elems:
+            image = job_elem.find('img')
+            # print(f'{formatted_name}: {image["src"]}')
+            logo_urls[name] = image['src']
+            worked += 1
+    
+    except:
+        print(f'{formatted_name}: DID NOT WORK')
+        failed += 1
+
+with open("logos.json", "w") as write_file:
+    json.dump(logo_urls, write_file)
+
+print(f'Worked: {worked}, Failed: {failed}')
